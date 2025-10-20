d9b61b3c13580f2ef0d04e2f10a8dfcd860e8a7a
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-10-20 11:15:22 -0300

    Score controller tests passing

--- src/test/java/com/devsuperior/dsmovie/controllers/ScoreControllerRA.java
+++ src/test/java/com/devsuperior/dsmovie/controllers/ScoreControllerRA.java
@@ -1,26 +1,80 @@
 package com.devsuperior.dsmovie.controllers;
 
+import com.devsuperior.dsmovie.tests.TokenUtil;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
 public class ScoreControllerRA {
 
-    private Long notExistingMovieId;
+    private String adminUsername, adminPassword, adminToken;
+    private Map<String, Object> postScoreInstance;
+
 
     @BeforeEach
     public void setUp() {
-        
+        baseURI = "http://localhost:8080";
+        adminUsername = "maria@gmail.com";
+        adminPassword = "123456";
+        adminToken = TokenUtil.obtainAccessToken(adminUsername, adminPassword);
+
+        postScoreInstance = new HashMap<>();
+        postScoreInstance.put("movieId", 1);
+        postScoreInstance.put("score", 4);
     }
 	
 	@Test
-	public void saveScoreShouldReturnNotFoundWhenMovieIdDoesNotExist() throws Exception {		
+	public void saveScoreShouldReturnNotFoundWhenMovieIdDoesNotExist() throws Exception {
+        postScoreInstance.put("movieId", 100);
+
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + adminToken)
+                .body(postScoreInstance)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .put("/scores")
+                .then()
+                .statusCode(404);
 	}
 	
 	@Test
 	public void saveScoreShouldReturnUnprocessableEntityWhenMissingMovieId() throws Exception {
+        postScoreInstance.put("movieId", null);
+
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + adminToken)
+                .body(postScoreInstance)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .put("/scores")
+                .then()
+                .statusCode(422);
 	}
 	
 	@Test
-	public void saveScoreShouldReturnUnprocessableEntityWhenScoreIsLessThanZero() throws Exception {		
+	public void saveScoreShouldReturnUnprocessableEntityWhenScoreIsLessThanZero() throws Exception {
+        postScoreInstance.put("score", -4);
+
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + adminToken)
+                .body(postScoreInstance)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .put("/scores")
+                .then()
+                .statusCode(422)
+                .body("errors.message[0]", equalTo("Valor mínimo 0"));
 	}
 }
